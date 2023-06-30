88d5c3f143c2a70ec0f433c06801748de6730850
Michał Koczewski <m.k0czewsk11@gmail.com>
2023-06-30 21:30:10 +0200

    Initial commit

---
+++ components/ExploreContainer.tsx
@@ -0,0 +1,73 @@
+import { IonText ,IonButton, useIonViewWillEnter, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCol, IonGrid, IonRow } from '@ionic/react';
+import React, { useState } from 'react';
+
+
+interface ContainerProps { }
+
+
+const ExploreContainer: React.FC<ContainerProps> = () => {
+
+  const [posts, setPosts] = useState([]); 
+  
+  useIonViewWillEnter(() => {
+     fetch('https://wolnelektury.pl/api/books/?format=json')
+        .then((res) => res.json())
+        .then((data) => {  
+          console.log(data);          
+          setPosts(data);                     
+        })
+        .catch((err) => {
+           console.log(err.message);
+        });
+  });
+
+  return (
+    <div className="container">      
+        {posts.map(p => {
+          return (
+            <IonGrid>
+              <IonRow>
+                <IonCol className='col'>  
+                  <IonCard className='card'>
+                    <IonCardHeader>
+                      <img src={p.simple_thumb}/>
+                      <IonCardSubtitle>
+                        {p.title}
+                      </IonCardSubtitle>                      
+                      <IonCardTitle>
+                        {p.author}
+                      </IonCardTitle>
+                      <IonCardContent>
+                        <IonText>
+                          Garunek: <i>{p.genre}</i>
+                        </IonText>
+                        <br></br>
+                        <IonText>
+                          Rodzaj literacki: <i>{p.kind}</i>
+                        </IonText>
+                        <br></br>
+                        <IonButton href={p.url} color={'primary'} expand='block' shape='round'>Szczegoly</IonButton>
+                      </IonCardContent>
+                    </IonCardHeader>
+                  </IonCard>
+                </IonCol>                                
+              </IonRow>
+            </IonGrid>
+          )})
+        }      
+    </div>
+  );
+};
+
+
+/*
+<IonItem key={p.id}>
+              <IonLabel>{p.title}</IonLabel>
+              <IonButton href={p.url} 
+               shape='round'> 
+              DETAILS 
+              </IonButton>                      
+            </IonItem>
+*/
+
+export default ExploreContainer;
